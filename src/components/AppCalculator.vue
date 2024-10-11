9e81f71b5b9d873ee0f35b1233f0ce691dad4d70
Raegan Beck <raeganbeck@Raegans-MacBook-Pro.local>
2024-10-10 18:13:21 -0600

    added square root functionality

--- src/components/AppCalculator.vue
+++ src/components/AppCalculator.vue
@@ -9,24 +9,28 @@
         <button @click="handleInputNumber(8)">8</button>
         <button @click="handleInputNumber(9)">9</button>
         <button @click="selectedOperation('/')">/</button>
+        <button @click="selectedOperation('sqr')">√</button>
       </div>
       <div class="row 2">
         <button @click="handleInputNumber(4)">4</button>
         <button @click="handleInputNumber(5)">5</button>
         <button @click="handleInputNumber(6)">6</button>
         <button @click="selectedOperation('*')">x</button>
+        <button @click="selectedOperation('sqr')">√</button>
       </div>
       <div class="row 3">
         <button @click="handleInputNumber(3)">3</button>
         <button @click="handleInputNumber(2)">2</button>
         <button @click="handleInputNumber(1)">1</button>
         <button @click="selectedOperation('-')">-</button>
+        <button @click="selectedOperation('sqr')">√</button>
       </div>
       <div class="row 4">
         <button @click="handleInputNumber(0)">0</button>
         <button @click="inputDecimal('.')">.</button>
         <button @click="performOperation()">=</button>
         <button @click="selectedOperation('+')">+</button>
+        <button @click="selectedOperation('sqr')">√</button>
       </div>
       <div class="row reset"><button @click="reset()">reset</button></div>
     </div>
@@ -70,8 +74,9 @@ export default {
     const selectedOperation = (operation) => {
       if (firstNumber.value) {
         currentOperation.value = operation;
-
-        onDisplay.value = `${onDisplay.value} ${operation}`;
+        if (currentOperation.value == "sqr") {
+          performOperation();
+        }
       } else {
         onDisplay.value = "must select number first";
       }
@@ -90,6 +95,8 @@ export default {
         result = firstNumber.value = (num1 * num2).toString();
       } else if (currentOperation.value == "/") {
         result = firstNumber.value = (num1 / num2).toString();
+      } else if (currentOperation.value == "sqr") {
+        result = firstNumber.value = Math.sqrt(num1).toString();
       } else {
         secondNumber.value == "";
         currentOperation.value == "";
@@ -129,8 +136,8 @@ export default {
   flex-direction: column;
   align-items: center;
   gap: 25px;
-  width: 300px;
-  height: 400px;
+  // width: 300px;
+  // height: 400px;
   padding: 15px;
   background-color: rgb(0, 89, 114);
   border-radius: 10px;
@@ -154,16 +161,17 @@ export default {
       width: 100%;
       display: flex;
       justify-content: space-around;
+      gap: 30px;
 
       button {
-        width: 50px;
+        width: 60px;
         height: 50px;
         border: none;
         border-radius: 5px;
 
         &:hover {
           cursor: pointer;
-          background-color: rgb(104, 104, 104, 0.7);
+          background-color: rgba(152, 152, 152, 0.7);
           border-radius: 10px;
         }
       }
